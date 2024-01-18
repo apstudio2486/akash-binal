01aae56158325824ea81909b19050bebe848f2f8
aakashandroid <108667175+aakashandroid@users.noreply.github.com>
2024-01-18 12:36:32 +0530

    add whatapp button and top scroll button

--- js/main.js
+++ js/main.js
@@ -1,33 +1,22 @@
 ;(function () {
 	
-	'use strict';
-
-	// Loading page
 	var loaderPage = function() {
 		$(".fh5co-loader").fadeOut("slow");
 	};
-	
-	// Loading page
 
-	var counter = function() {
+    var counter = function() {
 		const year = (new Date().getFullYear()) ;
         const weddingDate = new Date('march 11 2024 19:00:00 GMT+0300').getTime();
-        // countdown
         let timer = setInterval(function() {
 
-        // get today's date
         const today = new Date().getTime();
-
-        // get the difference
         const diff = weddingDate - today;
 
-        // math
         let days = Math.floor(diff / (1000 * 60 * 60 * 24));
         let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-        // display
         document.getElementById("timer").innerHTML =
             "<div class=\"days\"> \
         <div class=\"numbers\">" + days + "</div>days</div> \
@@ -39,12 +28,29 @@
         <div class=\"numbers\">" + seconds + "</div>seconds</div> \
         </div>";
 
-        }, 1000);
+    }, 1000);
+
 	};
-	
+    
+    var top = function(){$(".scroll").click(function(){
+        $("html,body").animate({
+            scrollTop:$(".top").offset().top
+        },"5000");
+        return false
+    }
+    )}
+    
 	$(function(){
-		loaderPage();
+        loaderPage();
+        top();
 		counter();
 	});
-
 }());
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
